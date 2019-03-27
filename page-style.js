a8a44a7609ef8547e8640c250d260938f043b1ca
Travis Craft <tlcraft@users.noreply.github.com>
2019-03-26 20:58:19 -0600

    Add show / hide logic to articles. Add toggle all.

--- page-style.js
+++ page-style.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    $(".toggle").click(function() {
-        $(this).closest('div.title-bar').next("article").toggle();
+    var showElements = true;
+
+    $('.toggle').click(function() {
+        $(this).closest('div.title-bar').next('article').toggle();
     });
+
+    $('.toggleAll').click(function() {
+        showElements = !showElements;
+        $(this).closest('section').children('section').children('article').toggle(showElements);
+    });    
 });
