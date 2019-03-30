5b1c7d0ec7ac8644238c5cec336b86ef012c25bc
Travis Craft <tlcraft@users.noreply.github.com>
2019-03-30 10:41:39 -0600

    Add topic links and collapse content on page load.

--- page-style.js
+++ page-style.js
@@ -8,5 +8,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     $('.toggleAll').click(function() {
         showElements = !showElements;
         $(this).closest('section').children('section').children('article').toggle(showElements);
-    });    
+    }); 
+    
+    $('.toggleAll').click();
 });
