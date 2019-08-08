b6d032e6af82de233ae8a5202d503e4bb7c7ff2e
Travis Craft <tlcraft@users.noreply.github.com>
2019-08-07 20:50:00 -0600

    Fix toggleAll logic.

--- page-style.js
+++ page-style.js
@@ -1,21 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    let showElements = true;
+    let showHtmlElements = true;
+    let showCssElements = true;
+    let showJsElements = true;
 
     $('.title-bar').click(function() {
         $(this).next('article').toggle();
     });    
 
-    $('.toggleAll').click(function() {
-        showElements = !showElements;
-        $(this).closest('section').children('section').children('article').toggle(showElements);
+
+    $('#html-examples .toggleAll').click(function() {
+        showHtmlElements = !showHtmlElements;
+        $(this).closest('section').children('section').children('article').toggle(showHtmlElements);
+    }); 
+
+    $('#css-examples .toggleAll').click(function() {
+        showCssElements = !showCssElements;
+        $(this).closest('section').children('section').children('article').toggle(showCssElements);
+    }); 
+
+    $('#js-examples .toggleAll').click(function() {
+        showJsElements = !showJsElements;
+        $(this).closest('section').children('section').children('article').toggle(showJsElements);
     }); 
     
     $('.topics-nav a').click(function() {
         openTopic($(this));
     });
 
-    $('.toggleAll').click();   
+    $('#html-examples .toggleAll').click();   
     $('#css-examples .toggleAll').click();
+    $('#js-examples .toggleAll').click();
 });
 
 function openTopic(link) {
